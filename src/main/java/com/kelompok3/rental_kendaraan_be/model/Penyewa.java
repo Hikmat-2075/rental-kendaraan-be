81ea5d4c92827db35103e98563fc43b0e2fcfd8f
kuaci1 <muhpaksip@gmail.com>
2025-05-27 16:18:19 +0700

    menambah class admin controller,penyewa

--- src/main/java/com/kelompok3/rental_kendaraan_be/model/Penyewa.java
+++ src/main/java/com/kelompok3/rental_kendaraan_be/model/Penyewa.java
@@ -1,11 +1,33 @@
 package com.kelompok3.rental_kendaraan_be.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 //Paksi
 @Entity
 @DiscriminatorValue("penyewa")
 public class Penyewa extends User {
-    
+    // Informasi tambahan (bisa ditambahkan ke form registrasi)
+    private String noTelepon;
+    private String alamat;
+
+    // Relasi ke transaksi
+    @OneToMany(mappedBy = "penyewa", cascade = CascadeType.ALL)
+    private List<Transaksi> transaksiList;
+
+    // Constructor dengan field tambahan
+    public Penyewa(String name, String email, String password, String noTelepon, String alamat) {
+        super(); // role default = penyewa
+        this.noTelepon = noTelepon;
+        this.alamat = alamat;
+    }
+
+    // Tambahan method (jika ingin logika dasar di entity)
+    public void tambahTransaksi(Transaksi transaksi) {
+        transaksi.setPenyewa(this);
+        this.transaksiList.add(transaksi);
+    }
 }
--- src/main/java/com/kelompok3/rental_kendaraan_be/repository/TransaksiRepository.java
+++ src/main/java/com/kelompok3/rental_kendaraan_be/repository/TransaksiRepository.java
@@ -1,6 +1,35 @@
 package com.kelompok3.rental_kendaraan_be.repository;
 
+import java.util.List;
+
+import com.kelompok3.rental_kendaraan_be.model.Transaksi;
+
 //Hikmat
 public class TransaksiRepository {
+
+    public long count() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    public long countByStatus(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'countByStatus'");
+    }
+
+    public Object findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    public List<Transaksi> findByStatus(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findByStatus'");
+    }
+
+    public Transaksi save(Transaksi transaksi) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
     
 }
