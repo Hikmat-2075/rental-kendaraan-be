e26ca72823c8d8305b441b1233232775e7ad3b94
hikmat <hikmatngrha@gmail.com>
2025-05-28 12:20:12 +0700

    memperbaiki admin

--- src/main/java/com/kelompok3/rental_kendaraan_be/model/Admin.java
+++ src/main/java/com/kelompok3/rental_kendaraan_be/model/Admin.java
@@ -3,31 +3,14 @@ package com.kelompok3.rental_kendaraan_be.model;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
-//Paksi
 @Entity
 @DiscriminatorValue("admin")
-public class Admin extends User{
-    // Default constructor
-     public Admin(String name, String email, String password) {
-        super(); // akan menyesuaikan constructor User nanti
-        
-    }
- // Method-method logis (kerangka kosong dulu)
+public class Admin extends User {
 
-    public void konfirmasiTransaksi(Long transaksiId) {
-        // TODO: panggil TransaksiService atau inject dan update status
+    public Admin() {
+        super();
     }
 
-    public void hapusKendaraan(Long kendaraanId) {
-        // TODO: panggil KendaraanService untuk menghapus
-    }
-
-    public void tambahKendaraan(Object kendaraan) {
-        // TODO: panggil KendaraanService atau repository
-    }
-
-    public void lihatRiwayat() {
-        // TODO: ambil data dari RiwayatTransaksiService
-    }
+    // Tambahkan method atau field khusus admin jika diperlukan
 
 }
