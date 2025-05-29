2f5ed24a55b1167373db97f821a8e9499e490327
raffa688 <raffacv9@gmail.com>
2025-05-29 13:13:15 +0700

    perubahan pada bagian transaksi,semua fungs yg diminta ada pada controller kecuali logika peminjaman dan perngembalian ada di service

--- src/main/java/com/kelompok3/rental_kendaraan_be/repository/TransaksiRepository.java
+++ src/main/java/com/kelompok3/rental_kendaraan_be/repository/TransaksiRepository.java
@@ -1,12 +1,12 @@
 package com.kelompok3.rental_kendaraan_be.repository;
 
+import com.kelompok3.rental_kendaraan_be.model.Transaksi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.kelompok3.rental_kendaraan_be.model.Transaksi;
-
-
+import java.util.List;
+///mengelola data transaksi kendaraan
 @Repository
 public interface TransaksiRepository extends JpaRepository<Transaksi, Long> {
-    // Tambahkan custom query jika dibutuhkan
+    List<Transaksi> findByUserId(Long userId);
 }
